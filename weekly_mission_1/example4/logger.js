a3034634a0008d24cb5ccc18b9366468c3169594
3rick <erick.dejesus1@alumno.buap.mx>
2022-05-08 19:54:34 -0500

    typo: week1-example4

--- weekly_mission_1/example4/logger.js
+++ weekly_mission_1/example4/logger.js
@@ -1,13 +1,14 @@
 class Logger {
-	constructor(name){
-		this.name = name
-	}
-	info (message) {
-		console.log(`[Objeto con nombre: ${this.name}] info: ${message}`)
-	}
+  constructor (name) {
+    this.name = name
+  }
 
-	verbose(message){
-		console.log(`[Objeto con nombre: ${this.name}] verbose: ${message}`)
-	}
+  info (message) {
+    console.log(`[Objeto con nombre: ${this.name}] info: ${message}`)
+  }
+
+  verbose (message) {
+    console.log(`[Objeto con nombre: ${this.name}] verbose: ${message}`)
+  }
 }
 module.exports = Logger
